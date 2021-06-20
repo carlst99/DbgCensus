4c3271e9de3b09747367a9ec1a92f112539ba282
Carl Stephens <carlst99@outlook.co.nz>
2021-06-21 11:12:19 +1200

    Add StringUtilTests

--- DbgCensus.Core/Utils/StringUtils.cs
+++ DbgCensus.Core/Utils/StringUtils.cs
@@ -4,6 +4,6 @@ namespace DbgCensus.Core.Utils
 {
     public static class StringUtils
     {
-        public static string JoinWithoutNullOrEmptyValues(char separator, params string[] value) => string.Join(separator, value.Where(str => !string.IsNullOrEmpty(str)));
+        public static string JoinWithoutNullOrEmptyValues(char separator, params string?[] value) => string.Join(separator, value.Where(str => !string.IsNullOrEmpty(str)));
     }
 }
---
+++ DbgCensus.Tests/Core/Utils/StringUtilsTests.cs
@@ -0,0 +1,16 @@
+﻿using DbgCensus.Core.Utils;
+using Xunit;
+
+namespace DbgCensus.Tests.Core.Utils
+{
+    public class StringUtilsTests
+    {
+        [Fact]
+        public void TestJoinWithoutNullOrEmptyValuesI()
+        {
+            string?[] values = new string?[] { "1", "", "2", null, "3" };
+
+            Assert.Equal("1,2,3", StringUtils.JoinWithoutNullOrEmptyValues(',', values));
+        }
+    }
+}
