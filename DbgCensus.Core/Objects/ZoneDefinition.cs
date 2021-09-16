4a57d3126246b8a016fed0824f476aab27b3102a
Carl Stephens <carlst99@outlook.co.nz>
2021-09-17 10:00:10 +1200

    Update zone definitions to include Tutorial2.

--- DbgCensus.Core/Objects/ZoneDefinition.cs
+++ DbgCensus.Core/Objects/ZoneDefinition.cs
@@ -1,4 +1,6 @@
-﻿namespace DbgCensus.Core.Objects
+﻿using System;
+
+namespace DbgCensus.Core.Objects
 {
     /// <summary>
     /// Defines the definition IDs of every supported zone in the Census API.
@@ -10,11 +12,22 @@
         Amerish = 6,
         Esamir = 8,
         Koltyr = 14,
+
+        /// <summary>
+        /// The old tutorial zone. Since replaced by <see cref="Tutorial2"/>.
+        /// </summary>
+        [Obsolete("This zone has been replaced by " + nameof(Tutorial2))]
         Tutorial = 95,
+
         VRTrainingNC = 96,
         VRTrainingTR = 97,
         VRTrainingVS = 98,
         Desolation = 361,
-        Sanctuary = 362
+        Sanctuary = 362,
+
+        /// <summary>
+        /// The new tutorial zone, introduced with the NPE update on 15/09/2021
+        /// </summary>
+        Tutorial2 = 364
     }
 }
