785beacbc54a01fc925fd63a1caa1ee70b6c115f
Carl Stephens <carlst99@outlook.co.nz>
2022-03-08 09:26:12 +1300

    Convert ZoneId.cs to readonly struct, and use computed properties for Instance and Definition.

--- DbgCensus.Core/Json/ZoneIdJsonConverter.cs
+++ DbgCensus.Core/Json/ZoneIdJsonConverter.cs
@@ -7,14 +7,15 @@ namespace DbgCensus.Core.Json;
 
 public class ZoneIdJsonConverter : JsonConverter<ZoneID>
 {
-    public override ZoneID? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override ZoneID Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         if (reader.TokenType is JsonTokenType.String && uint.TryParse(reader.GetString(), out uint value))
             return new ZoneID(value);
-        else if (reader.TokenType is JsonTokenType.Number && reader.TryGetUInt32(out value))
+
+        if (reader.TokenType is JsonTokenType.Number && reader.TryGetUInt32(out value))
             return new ZoneID(value);
-        else
-            return null;
+
+        return default;
     }
 
     public override void Write(Utf8JsonWriter writer, ZoneID value, JsonSerializerOptions options)
--- DbgCensus.Core/Objects/ZoneId.cs
+++ DbgCensus.Core/Objects/ZoneId.cs
@@ -5,53 +5,64 @@
 /// A zone ID is a <see cref="uint"/> where the upper two bytes represent the instance of the zone
 /// and the lower two bytes represent the <see cref="ZoneDefinition"/>.
 /// </summary>
-public record ZoneID
+public readonly struct ZoneID
 {
-    public static readonly ZoneID Default = new(0);
-
-    /// <summary>
-    /// Gets the instance of this zone.
-    /// </summary>
-    public ushort Instance { get; }
-
-    /// <summary>
-    /// Gets the definition of this zone.
-    /// </summary>
-    public ZoneDefinition Definition { get; }
-
     /// <summary>
     /// Gets the actual Census ID, a combination of the <see cref="Instance"/> and <see cref="Definition"/>
     /// </summary>
     public uint CombinedId { get; }
 
     /// <summary>
-    /// Initialises a new instance of the <see cref="ZoneID"/> record.
+    /// Gets the instance of this zone.
+    /// </summary>
+    public ushort Instance => (ushort)(CombinedId >> 16);
+
+    /// <summary>
+    /// Gets the definition of this zone.
+    /// </summary>
+    public ZoneDefinition Definition => (ZoneDefinition)(CombinedId & 0xffff);
+
+    /// <summary>
+    /// Initialises a new instance of the <see cref="ZoneID"/> struct.
     /// </summary>
     /// <param name="combinedId">The actual combined zone-id that was retrieved from Census.</param>
     public ZoneID(uint combinedId)
     {
         CombinedId = combinedId;
-        Definition = (ZoneDefinition)(combinedId & 0xffff);
-        Instance = (ushort)(combinedId >> 16);
     }
 
     /// <summary>
-    /// Initialises a new instance of the <see cref="ZoneID"/> record.
+    /// Initialises a new instance of the <see cref="ZoneID"/> struct.
     /// </summary>
     /// <param name="definition">The definition of the zone.</param>
     /// <param name="instance">The instance of the zone.</param>
     public ZoneID(ZoneDefinition definition, ushort instance)
     {
-        Definition = definition;
-        Instance = instance;
         CombinedId = ((uint)instance << 16) | (uint)definition;
     }
 
     public override string ToString()
         => $"{ Definition } (instance { Instance })";
 
-    public static explicit operator uint(ZoneID zoneId) => zoneId.CombinedId;
-    public static explicit operator ZoneDefinition(ZoneID zoneId) => zoneId.Definition;
+    public override bool Equals(object? obj)
+        => obj is ZoneID zoneId
+           && zoneId.CombinedId == CombinedId;
 
-    public static explicit operator ZoneID(uint combinedId) => new(combinedId);
+    public override int GetHashCode()
+        => CombinedId.GetHashCode();
+
+    public static explicit operator uint(ZoneID zoneId)
+        => zoneId.CombinedId;
+
+    public static explicit operator ZoneDefinition(ZoneID zoneId)
+        => zoneId.Definition;
+
+    public static explicit operator ZoneID(uint combinedId)
+        => new(combinedId);
+
+    public static bool operator ==(ZoneID z1, ZoneID z2)
+        => z1.Equals(z2);
+
+    public static bool operator !=(ZoneID z1, ZoneID z2)
+        => !z1.Equals(z2);
 }
