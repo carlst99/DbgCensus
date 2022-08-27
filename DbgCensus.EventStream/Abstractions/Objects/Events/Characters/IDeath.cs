06afb618b43f4da4374538b9ba60b4ed5c4149dd
Carl Stephens <carlst99@outlook.co.nz>
2022-08-27 22:10:24 +1200

    Implement AttackerTeamID, IsCritical and TeamID on IDeath.cs

--- DbgCensus.EventStream/Abstractions/Objects/Events/Characters/IDeath.cs
+++ DbgCensus.EventStream/Abstractions/Objects/Events/Characters/IDeath.cs
@@ -1,4 +1,6 @@
-﻿namespace DbgCensus.EventStream.Abstractions.Objects.Events.Characters;
+﻿using DbgCensus.Core.Objects;
+
+namespace DbgCensus.EventStream.Abstractions.Objects.Events.Characters;
 
 /// <summary>
 /// Represents a Death event.
@@ -21,6 +23,11 @@ public interface IDeath : IZoneEvent
     uint AttackerLoadoutID { get; }
 
     /// <summary>
+    /// Gets the current faction of the attacking character.
+    /// </summary>
+    FactionDefinition AttackerTeamID { get; }
+
+    /// <summary>
     /// Gets the ID of the vehicle used by the attacking character, or zero if a vehicle was not used.
     /// </summary>
     uint AttackerVehicleID { get; }
@@ -41,7 +48,17 @@ public interface IDeath : IZoneEvent
     uint CharacterLoadoutID { get; }
 
     /// <summary>
+    /// Unknown.
+    /// </summary>
+    bool IsCritical { get; }
+
+    /// <summary>
     /// Gets a value indicating whether or not the kill was a headshot.
     /// </summary>
     bool IsHeadshot { get; }
+
+    /// <summary>
+    /// Gets the current faction of the killed character.
+    /// </summary>
+    FactionDefinition TeamID { get; }
 }
--- DbgCensus.EventStream/Objects/Events/Characters/Death.cs
+++ DbgCensus.EventStream/Objects/Events/Characters/Death.cs
@@ -10,12 +10,15 @@ namespace DbgCensus.EventStream.Objects.Events.Characters;
 /// <param name="AttackerCharacterID">The ID of the attacking character.</param>
 /// <param name="AttackerFireMode">The fire mode of the attacking character.</param>
 /// <param name="AttackerLoadoutID">The loadout ID of the attacking character.</param>
+/// <param name="AttackerTeamID">Gets the current faction of the attacking character.</param>
 /// <param name="AttackerVehicleID">The ID of the vehicle used by the attacking character.</param>
 /// <param name="AttackerWeaponID">The ID of the weapon used by the attacking character.</param>
 /// <param name="CharacterID">The ID of the killed character.</param>
 /// <param name="CharacterLoadoutID">The loadout ID of the killed character.</param>
 /// <param name="EventName">The name of the event.</param>
+/// <param name="IsCritical">Unknown.</param>
 /// <param name="IsHeadshot">A value indicating whether the kill was a headshot.</param>
+/// <param name="TeamID">Gets the current faction of the killed character.</param>
 /// <param name="Timestamp">The time at which the event occured.</param>
 /// <param name="WorldID">The world on which the event occured.</param>
 /// <param name="ZoneID">The zone on which the event occured.</param>
@@ -24,12 +27,15 @@ public record Death
     ulong AttackerCharacterID,
     uint AttackerFireMode,
     uint AttackerLoadoutID,
+    FactionDefinition AttackerTeamID,
     uint AttackerVehicleID,
     uint AttackerWeaponID,
     ulong CharacterID,
     uint CharacterLoadoutID,
     string EventName,
+    bool IsCritical,
     bool IsHeadshot,
+    FactionDefinition TeamID,
     DateTimeOffset Timestamp,
     WorldDefinition WorldID,
     ZoneID ZoneID
