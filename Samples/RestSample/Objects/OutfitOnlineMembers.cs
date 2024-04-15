d15d1ef91775c483dcc49a13eafff56b5e05e1b1
Carl <carlst99@outlook.co.nz>
2024-04-16 00:53:59 +1200

    Fix improperly escaped XML comment

--- Samples/RestSample/Objects/OutfitOnlineMembers.cs
+++ Samples/RestSample/Objects/OutfitOnlineMembers.cs
@@ -6,7 +6,7 @@ namespace RestSample.Objects;
 
 /// <summary>
 /// Initialises a new instance of the <see cref="OutfitOnlineMembers"/> record.
-/// The query model for <see href="https://census.daybreakgames.com/get/ps2/outfit?outfit_id=37562651025751157&c:show=name,outfit_id,alias&c:join=outfit_member%5Einject_at:members%5Eshow:character_id%5Eouter:1%5Elist:1(character%5Eshow:name.first%5Einject_at:character%5Eouter:0%5Eon:character_id(characters_online_status%5Einject_at:online_status%5Eshow:online_status%5Eouter:0(world%5Eon:online_status%5Eto:world_id%5Eouter:0%5Eshow:world_id%5Einject_at:ignore_this))"/>.
+/// The query model for <see href="https://census.daybreakgames.com/get/ps2/outfit?outfit_id=37562651025751157&amp;c:show=name,outfit_id,alias&amp;c:join=outfit_member%5Einject_at:members%5Eshow:character_id%5Eouter:1%5Elist:1(character%5Eshow:name.first%5Einject_at:character%5Eouter:0%5Eon:character_id(characters_online_status%5Einject_at:online_status%5Eshow:online_status%5Eouter:0(world%5Eon:online_status%5Eto:world_id%5Eouter:0%5Eshow:world_id%5Einject_at:ignore_this))"/>.
 /// </summary>
 /// <param name="OutfitID">The ID of the outfit.</param>
 /// <param name="OutfitName">The name of the outfit.</param>
